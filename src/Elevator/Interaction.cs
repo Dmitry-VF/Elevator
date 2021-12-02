8f1114bd25436b39a569930bd8e5b634ac23005b
Dmitry-VF <fomedima@gmail.com>
2021-12-02 13:54:40 +0300

    stage 2

--- src/Elevator/Interaction.cs
+++ src/Elevator/Interaction.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Elevator
 {
@@ -6,53 +8,111 @@ namespace Elevator
     {
         public override void Introduction()
         {
-            Console.WriteLine("Which floor do you want to go to?");
+            Console.WriteLine("Enter the floor numbers where the elevator should stop, then enter Go");
             Console.WriteLine("Press X to quit");
         }
 
+        private List<int> Stops { get; set; } = new();
+        private List<int> VisitedStops { get; set; } = new();
+
         public override State ProcessInput()
         {
-            var input = Console.ReadLine();
-
-            if (input == "X")
+            while (true)
             {
-                Console.WriteLine("Have a good day!");
-                return State.End;
-            }
+                var input = Console.ReadLine();
 
-            if (int.TryParse(input, out var result) && result > 0 && result <= Elevator.FloorsCount && result != Elevator.CurrentFloor)
-            {
-                Console.WriteLine(" [>>][<<]");
-                if (Elevator.CurrentFloor < result)
+                if (string.Equals(input, "X", StringComparison.OrdinalIgnoreCase))
                 {
-                    for (var i = Elevator.CurrentFloor; i <= result; i++)
+                    Console.WriteLine("Have a good day!");
+                    return State.End;
+                }
+
+                if (string.Equals(input, "Go", StringComparison.OrdinalIgnoreCase))
+
+                {
+                    break;
+                }
+
+                if (int.TryParse(input, out var result))
+                {
+                    if (result > 0 && result <= Elevator.FloorsCount)
                     {
-                        Console.WriteLine($" [{i}]");
-                        Elevator.CurrentFloor = i;
+                        Stops.Add(result);
+                    }
+                    else
+                    {
+                        Console.WriteLine("There is no floor with such a number");
                     }
                 }
-                if (Elevator.CurrentFloor > result)
-                {
-                    for (var i = Elevator.CurrentFloor; i >= result; i--)
-                    {
-                        Console.WriteLine($" [{i}]");
-                        Elevator.CurrentFloor = i;
-                    }
-                }
-                Console.WriteLine(" [<<] [>>]");
-                return State.Interaction;
-            }
-            if (result == Elevator.CurrentFloor)
-            {
-                Console.WriteLine("You are already on this floor");
-                return State.Interaction;
-            }
-            else
-            {
-                Console.WriteLine("There is no floor with such a number");
             }
 
+            return StartMove();
+        }
+
+        private State StartMove()
+        {
+            Console.WriteLine(" [>>][<<]");
+
+            var nextStop = FindNextStop();
+
+            while (nextStop != 0)
+            {
+                MoveToClosestStop(nextStop);
+                nextStop = FindNextStop();
+            }
+
+            Stops.Clear();
+            VisitedStops.Clear();
+
+            Console.WriteLine(" [<<] [>>]");
             return State.Interaction;
+        }
+
+        private void MoveToClosestStop(int stop)
+        {
+            if (Elevator.CurrentFloor < stop)
+            {
+                RunInGivenDirection(stop, true);
+                return;
+            }
+            
+            if (Elevator.CurrentFloor > stop)
+            {
+                RunInGivenDirection(stop, false);
+                return;
+            }
+        }
+
+        private void RunInGivenDirection(int stop, bool direction)
+        {
+            // if direction is true, it will move up, otherwise down
+            var number = direction ? 1 : -1;
+
+            for (int i = Elevator.CurrentFloor; direction ? i <= stop : i >= stop; i+= number)
+            {
+                Console.WriteLine($" [{i}]");
+
+                var stopsLeft = Stops.Except(VisitedStops).ToArray();
+
+                if (Stops.Contains(i) && !VisitedStops.Contains(i) && stopsLeft.Length != 1)
+                {
+                    Console.WriteLine(" [<<] [>>]");
+                    Console.WriteLine(" [>>][<<]");
+                }
+                if (Stops.Contains(i))
+                {
+                    VisitedStops.Add(i);
+                }
+
+                Elevator.CurrentFloor = i;
+            }
+        }
+
+        private int FindNextStop()
+        {
+            var stopsListExceptVisited = Stops.Except(VisitedStops);
+            var closestStop = stopsListExceptVisited.OrderBy(stop => Math.Abs(Elevator.CurrentFloor - stop)).FirstOrDefault();
+            return closestStop;
         }
     }
 }
